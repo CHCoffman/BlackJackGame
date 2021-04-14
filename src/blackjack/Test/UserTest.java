74d3d65f70103940b771d5cb7b968aef0bb6460d
Colten <coltenhcoffman@gmail.com>
2021-04-14 13:03:50 -0500

    Testing for deck, stubs for hand, user, card and game

--- src/blackjack/Test/UserTest.java
+++ src/blackjack/Test/UserTest.java
@@ -1,35 +1,21 @@
 package blackjack.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import blackjack.model.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-class ModelTests {
-	
-	Card card = new Card(5, "clubs");
+class UserTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 	@Before
 	public void setUp() throws Exception 
-	{		
+	{	
 	}
 	
-	@Test
-	public void testCardCreated() throws Exception{
-		assertNotNull(card);
-	}
 	@After
 	public void tearDown() throws Exception 
 	{
-		card = null;
 		System.gc();
 	}
-
-
 }
