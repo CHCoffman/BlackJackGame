8744906116f72b317d68d221a2edcdbed3c1b604
Colten <coltenhcoffman@gmail.com>
2021-04-15 17:11:13 -0500

    tests

---
+++ src/blackjack/Test/CombinedTests.java
@@ -0,0 +1,14 @@
+package blackjack.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CombinedTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/blackjack/model/User.java
+++ src/blackjack/model/User.java
@@ -1,10 +1,8 @@
 package blackjack.model;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 =======
 import java.util.*;
->>>>>>> CCTest
 
 /* More to be implemented for class User  */
 public class User extends Hand {
