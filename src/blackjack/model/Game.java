4fa29a3fb27aeeabb5bec6342a3545046ec6e25d
ColbyIsaacks <50007919+ColbyIsaacks@users.noreply.github.com>
2021-04-27 17:56:46 -0500

    Added a basis for the Split function

--- src/blackjack/model/Game.java
+++ src/blackjack/model/Game.java
@@ -1,5 +1,7 @@
 package blackjack.model;
 
+import java.util.ArrayList;
+
 public class Game {
     public Game() {
         // Initialize game state
@@ -14,4 +16,24 @@ public class Game {
     // Make these Users
     public int Player;
     public int Dealer;
+    
+    
+    public static ArrayList<Hand> Split(Hand hand){
+		
+		ArrayList<Hand> tmp = new ArrayList<Hand>();
+		Hand tmpHand = null;
+		
+		
+		tmpHand = new Hand();
+		tmpHand.getHand().add(hand.getHand().get(1));
+		hand.getHand().remove(1);
+		
+		
+		tmp.add(hand);
+		tmp.add(tmpHand);
+		
+		return tmp;
+		
+	}
+    
 }
